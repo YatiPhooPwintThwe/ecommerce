1d91f8e1ea8219993b9185fdeb6173a578917e95
Yati Phoo Pwint Thwe <134662520+YatiPhooPwintThwe@users.noreply.github.com>
2025-08-21 21:50:45 +0800

    Update coupon.mutation.js

--- frontend/src/graphql/mutation/coupon.mutation.js
+++ frontend/src/graphql/mutation/coupon.mutation.js
@@ -2,11 +2,8 @@ import { gql } from "@apollo/client";
 export const VALIDATE_COUPON = gql`
   mutation ValidationCoupon($code: String!) {
     validateCoupon(code: $code) {
-      __typename
       code
       discountPercentage
-      redeemed
-      isActive
       message
     }
   }
